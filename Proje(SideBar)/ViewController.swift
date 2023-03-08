b8150f8daf8b3eeb73e3c98198eabcf98bf66285
TahirBayraktar <111084545+TahirBayraktar@users.noreply.github.com>
2023-03-08 15:26:24 +0300

    Initial Commit

---
+++ Proje(SideBar)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proje(SideBar)
+//
+//  Created by Tahir Bayraktar on 8.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
